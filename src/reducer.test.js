3fa959e2e564190e9104236b62850e2eaa0313b5
Quirozdev <luisdevv232@gmail.com>
2023-10-12 14:47:24 -0700

    uploading exercises 1 and 2

---
+++ src/reducer.test.js
@@ -0,0 +1,106 @@
+import deepFreeze from 'deep-freeze';
+import counterReducer from './reducer';
+
+describe('unicafe reducer', () => {
+  let initialState;
+
+  beforeEach(() => {
+    initialState = {
+      good: 0,
+      ok: 0,
+      bad: 0,
+    };
+  });
+
+  test('should return a proper initial state when called with undefined state', () => {
+    const state = {};
+    const action = {
+      type: 'DO_NOTHING',
+    };
+
+    const newState = counterReducer(undefined, action);
+    expect(newState).toEqual(initialState);
+  });
+
+  test('good is incremented', () => {
+    const action = {
+      type: 'GOOD',
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+
+    expect(state).toEqual(initialState);
+
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0,
+    });
+  });
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK',
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+
+    const newState = counterReducer(state, action);
+
+    expect(state).toEqual(initialState);
+
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0,
+    });
+  });
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD',
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+
+    const newState = counterReducer(state, action);
+
+    expect(state).toEqual(initialState);
+
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1,
+    });
+  });
+
+  test('all feedback is reset to zero', () => {
+    const action = {
+      type: 'ZERO',
+    };
+
+    initialState = {
+      good: 54,
+      ok: 3,
+      bad: 12,
+    };
+
+    const state = initialState;
+
+    deepFreeze(state);
+
+    const newState = counterReducer(state, action);
+
+    expect(state).toEqual(initialState);
+
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0,
+    });
+  });
+});
